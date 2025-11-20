1f6db91f484fa1fab9726f024e53553e09851483
nesmanpro <nesmanpro@gmail.com>
2025-11-20 01:41:37 +0100

    removing old skeleton

--- components/skeletons/SkeletonDetails.tsx
+++ components/skeletons/SkeletonDetails.tsx
@@ -1,19 +1,7 @@
-import { SkeletonCard } from "./SkeletonCard";
-import { SkeletonButton } from "./SkeletonButton";
 import { Skeleton } from "../ui/skeleton";
 
 export default function SkeletonDetails() {
   return (
-    // <div className="flex">
-    //   <div className=" grid grid-cols-4 gap-4">
-    //     <SkeletonCard />
-    //   </div>
-
-    //   <div className="mt-6 flex gap-4">
-    //     <SkeletonButton />
-    //     <SkeletonButton />
-    //   </div>
-    // </div>
     <main className="flex flex-col md:flex-row items-center w-full mb-20">
       <div className="w-[300px] md:w-64 aspect-square flex-1/2 md:flex-1/3 flex flex-col items-center">
         <Skeleton className="h-72 w-[250px]" />
